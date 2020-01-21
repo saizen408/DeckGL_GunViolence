c38671a23c72642f6fa7affe48a6e11707a9fc8f
Saizen408 <llkoole7@gmail.com>
2020-01-20 21:13:35 -0800

    reconfigure scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 const path = require("path");
 const webpack = require("webpack");
-// const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
-// if (process.env.NODE_ENV === "test") {
-//   require("dotenv").config({ path: ".env.test" });
-// } else if (process.env.NODE_ENV === "development") {
-//   require("dotenv").config({ path: ".env.development" });
-// }
+if (process.env.NODE_ENV === "test") {
+  require("dotenv").config({ path: ".env.test" });
+} else if (process.env.NODE_ENV === "development") {
+  require("dotenv").config({ path: ".env.development" });
+}
 
 module.exports = env => {
   const isProduction = env === "production";
-  // const CSSExtract = new ExtractTextPlugin("style.css");
+  const CSSExtract = new ExtractTextPlugin("style.css");
 
   return {
     entry: ["babel-polyfill", "./src/index.js"],
@@ -26,46 +26,24 @@ module.exports = env => {
           loader: "babel-loader",
           test: /\.js$/,
           exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/,
+          use: CSSExtract.extract({
+            use: [
+              { loader: "css-loader", options: { sourceMap: true } },
+              { loader: "sass-loader", options: { sourceMap: true } }
+            ]
+          })
         }
-        // {
-        //   test: /\.s?css$/,
-        //   use: CSSExtract.extract({
-        //     use: [
-        //       { loader: "css-loader", options: { sourceMap: true } },
-        //       { loader: "sass-loader", options: { sourceMap: true } }
-        //     ]
-        //   })
-        // }
       ]
+    },
+    plugins: [CSSExtract],
+    devtool: isProduction ? "source-map" : "inline-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      historyApiFallback: true,
+      publicPath: "/dist/"
     }
-    // plugins: [
-    //   CSSExtract,
-    //   new webpack.DefinePlugin({
-    //     "process.env.FIREBASE_API_KEY": JSON.stringify(
-    //       process.env.FIREBASE_API_KEY
-    //     ),
-    //     "process.env.FIREBASE_AUTH_DOMAIN": JSON.stringify(
-    //       process.env.FIREBASE_AUTH_DOMAIN
-    //     ),
-    //     "process.env.FIREBASE_DATABASE_URL": JSON.stringify(
-    //       process.env.FIREBASE_DATABASE_URL
-    //     ),
-    //     "process.env.FIREBASE_PROJECT_ID": JSON.stringify(
-    //       process.env.FIREBASE_PROJECT_ID
-    //     ),
-    //     "process.env.FIREBASE_STORAGE_BUCKET": JSON.stringify(
-    //       process.env.FIREBASE_STORAGE_BUCKET
-    //     ),
-    //     "process.env.FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(
-    //       process.env.FIREBASE_MESSAGING_SENDER_ID
-    //     )
-    //   })
-    // ],
-    // devtool: isProduction ? "source-map" : "inline-source-map",
-    // devServer: {
-    //   contentBase: path.join(__dirname, "public"),
-    //   historyApiFallback: true,
-    //   publicPath: "/dist/"
-    // }
   };
 };

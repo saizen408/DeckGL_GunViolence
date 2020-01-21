9084cdad38a2109d5667bca948a02ec11f488e9e
Saizen408 <llkoole7@gmail.com>
2020-01-20 21:59:39 -0800

    script configuration

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,6 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
-if (process.env.NODE_ENV === "test") {
-  require("dotenv").config({ path: ".env.test" });
-} else if (process.env.NODE_ENV === "development") {
-  require("dotenv").config({ path: ".env.development" });
-}
-
 module.exports = env => {
   const isProduction = env === "production";
   const CSSExtract = new ExtractTextPlugin("style.css");
@@ -26,20 +20,10 @@ module.exports = env => {
           loader: "babel-loader",
           test: /\.js$/,
           exclude: /node_modules/
-        },
-        {
-          test: /\.s?css$/,
-          use: CSSExtract.extract({
-            use: [
-              { loader: "css-loader", options: { sourceMap: true } },
-              { loader: "sass-loader", options: { sourceMap: true } }
-            ]
-          })
         }
       ]
     },
-    plugins: [CSSExtract],
-    devtool: isProduction ? "source-map" : "inline-source-map",
+
     devServer: {
       contentBase: path.join(__dirname, "public"),
       historyApiFallback: true,

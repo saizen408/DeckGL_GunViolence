4c1663de145dee1a294079bc7827c5591521073e
Saizen408 <llkoole7@gmail.com>
2020-01-22 23:29:43 -0800

    update to babel 7 and webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,51 @@
 const path = require("path");
 const webpack = require("webpack");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 module.exports = env => {
   const isProduction = env === "production";
-  const CSSExtract = new ExtractTextPlugin("style.css");
 
   return {
-    entry: ["babel-polyfill", "./src/index.js"],
+    entry: {
+      main: ["babel-polyfill", "./src/index.js"]
+    },
+    // plugins: [
+    //   // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+    //   new CleanWebpackPlugin(),
+    //   new HtmlWebpackPlugin({
+    //     title: "Caching"
+    //   })
+    // ],
     output: {
-      path: path.join(__dirname, "public", "dist"),
+      path: path.resolve(__dirname, "public", "dist"),
       filename: "bundle.js"
     },
+
+    // optimization: {
+    //   runtimeChunk: "single"
+    // },
     module: {
       rules: [
         {
-          loader: "babel-loader",
-          test: /\.js$/,
-          exclude: /node_modules/
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: ["@babel/plugin-transform-runtime"]
+            }
+          }
         }
       ]
     },
 
     devServer: {
-      contentBase: path.join(__dirname, "public"),
-      historyApiFallback: true,
-      publicPath: "/dist/"
+      contentBase: "./public/dist"
     }
   };
 };
